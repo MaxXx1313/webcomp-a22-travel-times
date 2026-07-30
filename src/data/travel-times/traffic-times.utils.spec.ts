d34e05b2bc2706c402a75dec6a69c566304b352e
Maksim Chertkov <maxch@unl.solutions>
2026-07-31 00:24:00 +0200

    #5 some tests

--- src/data/travel-times/traffic-times.utils.spec.ts
+++ src/data/travel-times/traffic-times.utils.spec.ts
@@ -36,6 +36,106 @@ describe("TrafficTimesUtils", () => {
       }
     });
 
+    it('should parse level 3', () => {
+
+      const inputArr = [
+        "traffico rallentato con code",
+        " traffico rallentato con code ",
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseTrafficLevel({mvalue: inp} as any)).toBe(3);
+      }
+    });
+
+    it('should parse level 4', () => {
+
+      const inputArr = [
+        "code a tratti",
+        " code a tratti ",
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseTrafficLevel({mvalue: inp} as any)).toBe(4);
+      }
+    });
+
+    it('should parse level 5', () => {
+
+      const inputArr = [
+        "traffico critico",
+        " traffico critico ",
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseTrafficLevel({mvalue: inp} as any)).toBe(5);
+      }
+    });
+
+    it('should parse level unknown', () => {
+
+      const inputArr = [
+        "some unknonwn value",
+        " asdasd ",
+        null,
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseTrafficLevel({mvalue: inp} as any)).toBe(-1);
+      }
+    });
+  });
+
+  describe('parseVehicleType', () => {
+    it('should parse vehicle type light', () => {
+
+      const inputArr = [
+        "lds_leggeri_desc",
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseVehicleType({tname: inp} as any)).toBe('light');
+      }
+    });
+
+    it('should parse vehicle type heavy', () => {
+
+      const inputArr = [
+        "lds_pesanti_desc",
+      ];
+
+      for (const inp of inputArr) {
+        expect(TrafficTimesUtils.__parseVehicleType({tname: inp} as any)).toBe('heavy');
+      }
+    });
+  });
+
+  describe('parseStationCode', () => {
+    it('should parse scode L3', () => {
+
+      expect(TrafficTimesUtils.__parseStationCode({scode: "02_A22A22_01-00680_01-00679_DX"} as any))
+        .toStrictEqual({from:'00680', to:'00679', source: "tollgate"});
+    });
+
+    it('should parse scode L2', () => {
+
+      expect(TrafficTimesUtils.__parseStationCode({scode: "1865-1864"} as any))
+        .toStrictEqual({from:'1865', to:'1864', source: "tollgate"});
+    });
+
+    it('should parse scode L0', () => {
+
+      expect(TrafficTimesUtils.__parseStationCode({scode: "urn:linkstation:a22:tvcc:28", sname:"K1-K2"} as any))
+        .toStrictEqual({from:'k1', to:'k2', source: "tvcc"});
+    });
+
+
+    it('should not parse scode L2', () => {
+
+      expect(TrafficTimesUtils.__parseStationCode({scode: "somewhat"} as any))
+        .toStrictEqual(null);
+    });
+
   });
 
 });
